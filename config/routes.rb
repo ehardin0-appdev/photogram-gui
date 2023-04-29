f528fa8536165eeb78aad06ec9b98c114df68ab6
Elle Harding <ehardin0@chicagobooth.edu>
2023-04-29 16:39:24 +0000

    Users Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index" })
 
-  get("/users/:xxx", { :controller => "users", :action => ""})
+  get("/users/:path_username", { :controller => "users", :action => "show"})
 end
