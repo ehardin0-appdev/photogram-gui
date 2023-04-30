3783d184a47608414b8b2bf0ccda6e33bc40ec6c
Elle Harding <ehardin0@chicagobooth.edu>
2023-04-30 17:14:00 +0000

    Photo create commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   get("/delete_photo/:toast_id", { :controller => "photos", :action => "delete" })
 
+  get("/insert_photo", { :controller => "photos", :action => "create" } )
 end
